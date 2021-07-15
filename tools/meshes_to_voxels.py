f3d7894bcf269b5bf7140e12cf68c907ea612758
gleb <glebk98@gmail.com>
2021-07-16 00:47:51 +0300

    WIP

---
+++ tools/meshes_to_voxels.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import trimesh as tr
+from trimesh.voxel import creation # why doesn't it work without this import? 
+import numpy as np
+
+def get_voxel_grid_from_mesh(path_to_mesh, resolution=64):
+    mesh = tr.load(path_to_mesh)
+    scale = 1/max(mesh.extents)
+    x = y = z = 0
+    for index, vertex in enumerate(mesh.vertices):       
+        mesh.vertices[index] = vertex * scale # normalization
+        x += mesh.vertices[index][0]
+        y += mesh.vertices[index][1]
+        z += mesh.vertices[index][2]
+
+    x_avrg = x/len(mesh.vertices)
+    y_avrg = y/len(mesh.vertices)
+    z_avrg = z/len(mesh.vertices)
+
+    for index, vertex in enumerate(mesh.vertices): # centering
+        mesh.vertices[index][0] -= x_avrg
+        mesh.vertices[index][1] -= y_avrg
+        mesh.vertices[index][2] -= z_avrg
+
+    # second argument is the voxel size - for some reason I had to devide 1 by 63 to get 64 voxels (still I got 65 voxels on one of the meshes) 
+    grid = tr.voxel.creation.voxelize(mesh, 0.015873) 
+    grid = grid.matrix # the actual values for the voxels as a numpy array
+
+    new_grid = [[[False] * resolution] * resolution] * resolution
+    new_grid = np.array(new_grid)
+
+    d1, d2, d3 = grid.shape
+    for i in range(d1 if d1 <= resolution else resolution):
+        for j in range(d2 if d2 <= resolution else resolution):
+            for k in range(d3 if d3 <= resolution else resolution):
+                new_grid[i][j][k] = grid[i][j][k]
+
+    return new_grid
+
+
+path_to_dataset = r"C:\Projects\mvcnn_pytorch\ModelNet40"
+voxelized_dataset_path = r"ModelNet40Voxelized"
+
+if not os.path.exists(voxelized_dataset_path):
+    os.makedirs(voxelized_dataset_path)
+
+#classnames=['airplane','bathtub','bed','bench','bookshelf','bottle','bowl','car','chair',
+#                         'cone','cup','curtain','desk','door','dresser','flower_pot','glass_box',
+#                         'guitar','keyboard','lamp','laptop','mantel','monitor','night_stand',
+#                         'person','piano','plant','radio','range_hood','sink','sofa','stairs',
+#                         'stool','table','tent','toilet','tv_stand','vase','wardrobe','xbox']
+
+classnames=['bookshelf']
+
+for classname in classnames:
+    if not os.path.exists(os.path.join(voxelized_dataset_path, classname)):
+        os.makedirs(os.path.join(voxelized_dataset_path, classname))
+    print("")
+    for split in ["train", "test"]:
+        print("")
+        i = 0
+        n_files = len(os.listdir(os.path.join(path_to_dataset, classname, split)))
+        for filename in os.listdir(os.path.join(path_to_dataset, classname, split)):
+            if not os.path.exists(os.path.join(voxelized_dataset_path, classname, split)):
+                os.makedirs(os.path.join(voxelized_dataset_path, classname, split))
+
+            if not os.path.exists(os.path.join(voxelized_dataset_path, classname, split, filename[:-4]+".npy")):
+                if not os.path.exists(os.path.join(voxelized_dataset_path, classname, split, filename[:-4]+".npy")):
+                    try:
+                        grid = get_voxel_grid_from_mesh(os.path.join(path_to_dataset, classname, split, filename))
+                        np.save(os.path.join(voxelized_dataset_path, classname, split, filename[:-4]), grid)        
+                    except Exception as ex:
+                        i += 1
+                        print(ex)
+                        print("\File {} out of {} in {} wasn't written due to memory error!\n".format(i, n_files, os.path.join(voxelized_dataset_path, classname, split)))                        
+                        continue        
+
+            i += 1
+            sys.stdout.write("\rWrote file {} out of {} in {}".format(i, n_files, os.path.join(voxelized_dataset_path, classname, split)))
+            sys.stdout.flush()
---
+++ tools/the_dataset.py
@@ -0,0 +1,78 @@
+import torch
+import os
+from PIL import Image
+from torchvision import transforms
+import numpy as np
+
+class TheDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path_2D, data_path_3D, num_views=12, num_models = 0):
+        self.classnames=['airplane','bathtub','bed','bench','bookshelf','bottle','bowl','car','chair',
+                    'cone','cup','curtain','desk','door','dresser','flower_pot','glass_box',
+                    'guitar','keyboard','lamp','laptop','mantel','monitor','night_stand',
+                    'person','piano','plant','radio','range_hood','sink','sofa','stairs',
+                    'stool','table','tent','toilet','tv_stand','vase','wardrobe','xbox']
+        self.data_path_2D = data_path_2D
+        self.data_path_3D = data_path_3D
+        self.num_views = num_views
+        self.filepaths_2D = []
+        self.filepaths_3D = []
+
+        self.transform = transforms.Compose([
+                transforms.RandomHorizontalFlip(), # do we want this? 
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], # not sure what those are doing
+                                     std=[0.229, 0.224, 0.225])
+            ])
+
+        for classname in self.classnames:    
+            for split in ["train", "test"]:
+                for filename in os.listdir(os.path.join(self.data_path_2D, classname, split)):                
+                    self.filepaths_2D.append(os.path.join(self.data_path_2D, classname, split, filename))    
+
+        if num_models != 0: # Use only a part of the dataset
+            self.filepaths_2D = self.filepaths_2D[:min(num_models,len(self.filepaths_2D))]
+
+        # Select subset for different number of views
+        if self.num_views == 12:
+            pass
+        elif self.num_views in [6,4,3,2,1]:
+            stride = int(12/self.num_views)
+            self.filepaths_2D = self.filepaths_2D[::stride]
+        else:
+            raise Exception("Invalid number of views")
+
+        # 3D
+        for classname in self.classnames:    
+            for split in ["train", "test"]:
+                for filename in os.listdir(os.path.join(self.data_path_3D, classname, split)):                
+                    self.filepaths_3D.append(os.path.join(self.data_path_3D, classname, split, filename))
+
+    def __len__(self):
+        return int(len(self.filepaths_3D)) 
+
+    def __getitem__(self, idx):
+        # 2D        
+        path = self.filepaths_2D[idx*self.num_views - self.num_views + 1] 
+        path = os.path.normpath(path) 
+        class_name = path.split(os.sep)[-3]
+        #class_id = self.classnames.index(class_name)                        
+
+        imgs = []
+        for i in range(self.num_views):
+            im = Image.open(self.filepaths_2D[idx*self.num_views - self.num_views + 1 + i]).convert('RGB')            
+            if self.transform:
+                im = self.transform(im)
+            imgs.append(im)
+        
+        stacked_images = torch.stack(imgs)
+
+        # 3D
+        grid = np.load(self.filepaths_3D[idx])
+        grid = torch.from_numpy(grid)
+        print(grid.size())
+        return (class_name, stacked_images, grid)
+
+
+data = TheDataset(data_path_2D=r"C:\Projects\mvcnn_pytorch\ModelNet40_2D", data_path_3D=r"C:\Projects\mvcnn_pytorch\ModelNet40", num_models=0)
+print(data.__len__())
+data.__getitem__(0)
